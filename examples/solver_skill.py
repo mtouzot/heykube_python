9695790afaef731cfb352e131bb6864386a70f03
mtouzot <martin.touzot@gmail.com>
2024-06-22 22:30:12 +0200

    Update Copyright in Python files

--- examples/solver_skill.py
+++ examples/solver_skill.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # ========================================
 # Copyright 2021 22nd Solutions, LLC
+# Copyright 2024 Martin TOUZOT
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
